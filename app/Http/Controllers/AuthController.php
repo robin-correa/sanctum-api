a3b7c2c279ee35c3dcdc151e68d6733cbafb3e53
Robin Correa <robin.correa21@gmail.com>
2021-05-22 17:36:32 +0800

    Add user auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'error' => 'Invalid credentials'
+            ]);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24);
+
+        return response(
+            ['token' => $token]
+        )->withCookie($cookie);
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('token');
+
+        return response(
+            ['message' => 'success']
+        )->withCookie($cookie);
+    }
+}
